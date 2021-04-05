ffb12d4d4f5b79a151cd1e6ffe9b90d1c762f54c
Michael Romay <amerror@gmail.com>
2021-04-05 13:12:13 -0700

    added mp3 player

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+// import 'react-h5-audio-player/lib/styles.less' Use LESS
+// import 'react-h5-audio-player/src/styles.scss' Use SASS
+
+// const Player = () => (
+//   <AudioPlayer
+//     autoPlay
+//     src="http://example.com/audio.mp3"
+//     onPlay={e => console.log("onPlay")}
+//     // other props here
+//   />
+// );
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <><h1>Hello World</h1>
+  <AudioPlayer
+  autoPlay
+  src="./example/mp3/example.mp3"
+  onPlay={e => console.log("onPlay")}
+  // other props here
+/>
+</>
 }
