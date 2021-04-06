d5265da85a667ce0aa7fa42a7b826f026f9592ec
Michael Romay <amerror@gmail.com>
2021-04-05 19:40:18 -0700

    four song buttons added

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import { useSong, useHandleSong } from '../../hooks/DataProvider';
 import {
@@ -10,39 +10,9 @@ import {
 // import styles from 'react-h5-audio-player/lib/styles.css';
 import styles from './Player.css';
 
-// let handleSongs = useHandleSongs();
 const Player = () => {
   const song = useSong();
   const handleSong = useHandleSong();
-
-
-  // handleSong();
-  // const handleSongs = (thing) => {
-  //   // console.log(thing)
-  //   // event.preventDefault(); 
-  //   useHandleSongs(thing);
-  //   console.log('handleSongs', songs);
-    
-  // }
-
-
-  // useEffect(() => {
-  //   console.log('useEffect happened');
-  //   handleSong(songFour);
-  // }, []);
-
-  // useHandleSongs(songTwo);
-
-
-  // const makeSong = (thing) => {
-  //   useHandleSongs(thing);
-  //   console.log('hello')
-  // } 
-
-  // const handleClick = () => {
-  //   console.log('whatever')
-  //   makeSong('../mp3/example3.mp3');
-  // };
 
   return (
     <>
@@ -50,17 +20,28 @@ const Player = () => {
       <AudioPlayer
         className={styles}
         src={song.url}
-        onPlay={e => console.log('onPlay')}
+        onPlay={() => console.log('onPlay')}
       />
-      {/* <a href="songOne">uhhh</a> */}
       
-      {/* <button
-        onClick={handleSong}>
-          Click here!
-      </button> */}
-      {/* <button onClick={() => handleClick()}>
-          Click here!
-    </button> */}
+      <button
+        onClick={()=>handleSong(songOne)}>
+          song one
+      </button>
+
+      <button
+        onClick={()=>handleSong(songTwo)}>
+          song two
+      </button>
+
+      <button
+        onClick={()=>handleSong(songThree)}>
+          song three
+      </button>
+
+      <button
+        onClick={()=>handleSong(songFour)}>
+          song four
+      </button>
     </>
   );
 };
