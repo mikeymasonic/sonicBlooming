6fc1f9282e174c612a3221d43f25ed5842db702c
Michael Romay <amerror@gmail.com>
2021-04-07 20:49:41 -0700

    fixed about

--- src/components/About/About.test.jsx
+++ src/components/About/About.test.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { DataProvider } from '../../hooks/DataProvider';
 import About from './About';
 
 describe('About component', () => {
   afterEach(() => cleanup());
   it('renders About', () => {
-    const { asFragment } = render(<About />);
+    const { asFragment } = render(
+      <Router>
+        <DataProvider>
+          <About />
+        </DataProvider>
+      </Router>
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
