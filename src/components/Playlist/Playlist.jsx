4560ca101f1716893b4953582f11cc4bc1c3f6cd
Michael Romay <amerror@gmail.com>
2021-04-12 19:16:53 -0700

    working state

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -10,12 +10,15 @@ const Playlist = () => {
   const mapLocation = useMapLocation();
   const handleSong = useHandleSong();
   // const song = useSong();
+
+  // if (mapLocation === song.mapLocation[0])
   const playlistNodes = songs.map((song) => {
     // return <p key={song.title}>hi</p>;
     if (mapLocation === song.mapLocation) {
-      
-      return <a key={song.title} onClick={() => handleSong(song)}>hii{song.title}</a>;
+      // handleSong(songs[0]);
+      return <a key={song.title} onClick={() => handleSong(song)}>{song.title}</a>;
     }
+    // handleSong(songs[1]);
   });
 
   return (
