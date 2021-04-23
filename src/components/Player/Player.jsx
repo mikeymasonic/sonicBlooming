9156fbd6cc243fedf4a66de4eb2e7e13d6177da7
Jena Boehm <boehmjena@gmail.com>
2021-04-22 23:35:16 -0700

    add colors, font, more styling on player/playlist

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -8,30 +8,29 @@ import styles from './Player.css';
 const Player = () => {
   const song = useSong();
   const playerVisible = usePlayerVisible();
+  const songUpper = song?.title.toUpperCase();
 
   return (
     <>
       <div className={styles.audioContainer}>
         <Playlist style={{ visibility: playerVisible ? 'visible' : 'hidden', height: playerVisible ? 100 : 0 }} />
-
-        <section className={styles.Player} style={{ visibility: playerVisible ? 'visible' : 'collapse', height: playerVisible ? 100 : 0 }}>
-          <section className={styles.title}>
-            {song?.mapLocation} - {song?.title}
+        <div className={styles.playerContainer} style={{ visibility: playerVisible ? 'visible' : 'hidden', height: playerVisible ? 100 : 0 }}>
+          <section className={styles.title}>{songUpper}</section>
+          <section className={styles.Player} style={{ visibility: playerVisible ? 'visible' : 'collapse', height: playerVisible ? 100 : 0 }}>
+            <AudioPlayer
+              className={styles.rhap_container}
+              src={song?.url}
+              customProgressBarSection={
+                [
+                  RHAP_UI.CURRENT_TIME,
+                  // eslint-disable-next-line react/jsx-key
+                  <section className={styles.slash}>/</section>,
+                  RHAP_UI.DURATION
+                ]
+              }
+            />
           </section>
-          <AudioPlayer
-            className={styles.rhap_container}
-            src={song?.url}
-            customProgressBarSection={
-              [
-                RHAP_UI.CURRENT_TIME,
-                // eslint-disable-next-line react/jsx-key
-                <section>/</section>,
-                RHAP_UI.DURATION
-              ]
-            }
-          />
-        </section>
-
+        </div>
         {!playerVisible && <p className={styles.instructions}>Select a garden to listen to:</p>} 
       </div>
     </>
