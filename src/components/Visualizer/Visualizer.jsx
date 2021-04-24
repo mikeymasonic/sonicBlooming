f566ed3d8a0a5bf06abb2ca16a60397dbb2527c8
Michael Romay <amerror@gmail.com>
2021-04-23 17:56:31 -0700

    visualizer works and you can close it, but theres a new bug with volume doubling each time you click on fullscreen

--- src/components/Visualizer/Visualizer.jsx
+++ src/components/Visualizer/Visualizer.jsx
@@ -11,8 +11,9 @@ const Visualizer = ({ forwardRef, close }) => {
   // const handlePlayerSuspend = useHandlePlayerSuspend();
 
   useEffect(() => {
-    console.log('propThing: ', forwardRef.current);
-    
+    // console.log('propThing: ', forwardRef.current);
+    const audio = forwardRef.current.audio.current;
+
     const centerX = 500;
     const centerY = 500;
     const radius = document.body.clientWidth <= 425 ? 120 : 160;
@@ -22,12 +23,14 @@ const Visualizer = ({ forwardRef, close }) => {
     const pointsDown = [];
     const pCircle = 2 * Math.PI * radius;
     const angleExtra = 90;
-    
+
     // -------------
     // Audio stuff
     // -------------
 
-    const context = new AudioContext();
+    audio.context = audio.context || new AudioContext();
+    const context = audio.context;
+
     // const context = window.AudioContext || window.webkitAudioContext;
     // const context = forwardRef.current.context.current;
 
@@ -56,29 +59,43 @@ const Visualizer = ({ forwardRef, close }) => {
 
     // Make a audio node
     // const audio = new Audio();
-    const audio = forwardRef.current.audio.current;
-    const source = context.createMediaElementSource(audio);
+
+    audio.source = audio.source || context.createMediaElementSource(audio);
+    const source = audio.source;
+
     source.connect(splitter);
     //comment this out to disable audio playback
     splitter.connect(context.destination);
 
     // Create points
-    for(let angle = 0; angle < 360; angle += interval) {
+    for (let angle = 0; angle < 360; angle += interval) {
       const distUp = 1.1;
       const distDown = 0.9;
-  
+
       pointsUp.push({
         angle: angle + angleExtra,
-        x: centerX + radius * Math.cos((-angle + angleExtra) * Math.PI / 180) * distUp,
-        y: centerY + radius * Math.sin((-angle + angleExtra) * Math.PI / 180) * distUp,
-        dist: distUp
+        x:
+          centerX +
+          radius * Math.cos(((-angle + angleExtra) * Math.PI) / 180) * distUp,
+        y:
+          centerY +
+          radius * Math.sin(((-angle + angleExtra) * Math.PI) / 180) * distUp,
+        dist: distUp,
       });
-  
+
       pointsDown.push({
         angle: angle + angleExtra + 5,
-        x: centerX + radius * Math.cos((-angle + angleExtra + 5) * Math.PI / 180) * distDown,
-        y: centerY + radius * Math.sin((-angle + angleExtra + 5) * Math.PI / 180) * distDown,
-        dist: distDown
+        x:
+          centerX +
+          radius *
+            Math.cos(((-angle + angleExtra + 5) * Math.PI) / 180) *
+            distDown,
+        y:
+          centerY +
+          radius *
+            Math.sin(((-angle + angleExtra + 5) * Math.PI) / 180) *
+            distDown,
+        dist: distDown,
       });
     }
     const canvas = canvasRef.current;
@@ -94,20 +111,20 @@ const Visualizer = ({ forwardRef, close }) => {
 
     function drawLine(points) {
       const origin = points[0];
-  
+
       ctx.beginPath();
       ctx.strokeStyle = 'rgba(255,255,255,0.5)';
       ctx.lineJoin = 'round';
       ctx.moveTo(origin.x, origin.y);
-  
+
       for (let i = 0; i < points.length; i++) {
         ctx.lineTo(points[i].x, points[i].y);
       }
-  
+
       ctx.lineTo(origin.x, origin.y);
       ctx.stroke();
     }
-  
+
     function connectPoints(pointsA, pointsB) {
       for (let i = 0; i < pointsA.length; i++) {
         ctx.beginPath();
@@ -119,41 +136,59 @@ const Visualizer = ({ forwardRef, close }) => {
     }
     function draw(dt) {
       requestAnimationFrame(draw);
-  
+
       if (running) {
         update(dt);
       }
-  
+
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+
       drawLine(pointsUp);
       drawLine(pointsDown);
       connectPoints(pointsUp, pointsDown);
     }
-  
+
     function update() {
       let audioIndex, audioValue;
-  
+
       // get the current audio data
       analyserL.getByteFrequencyData(audioDataArrayL);
       analyserR.getByteFrequencyData(audioDataArrayR);
-  
+
       for (let i = 0; i < pointsUp.length; i++) {
-        audioIndex = Math.ceil(pointsUp[i].angle * (bufferLengthL / (pCircle * 2))) | 0;
+        audioIndex =
+          Math.ceil(pointsUp[i].angle * (bufferLengthL / (pCircle * 2))) | 0;
         // get the audio data and make it go from 0 to 1
         audioValue = audioDataArrayL[audioIndex] / 255;
-  
+
         pointsUp[i].dist = 1.1 + audioValue * 0.8;
-        pointsUp[i].x = centerX + radius * Math.cos(-pointsUp[i].angle * Math.PI / 180) * pointsUp[i].dist;
-        pointsUp[i].y = centerY + radius * Math.sin(-pointsUp[i].angle * Math.PI / 180) * pointsUp[i].dist;
-  
-        audioIndex = Math.ceil(pointsDown[i].angle * (bufferLengthR / (pCircle * 2))) | 0;
+        pointsUp[i].x =
+          centerX +
+          radius *
+            Math.cos((-pointsUp[i].angle * Math.PI) / 180) *
+            pointsUp[i].dist;
+        pointsUp[i].y =
+          centerY +
+          radius *
+            Math.sin((-pointsUp[i].angle * Math.PI) / 180) *
+            pointsUp[i].dist;
+
+        audioIndex =
+          Math.ceil(pointsDown[i].angle * (bufferLengthR / (pCircle * 2))) | 0;
         // get the audio data and make it go from 0 to 1
         audioValue = audioDataArrayR[audioIndex] / 255;
-  
+
         pointsDown[i].dist = 0.9 + audioValue * 0.2;
-        pointsDown[i].x = centerX + radius * Math.cos(-pointsDown[i].angle * Math.PI / 180) * pointsDown[i].dist;
-        pointsDown[i].y = centerY + radius * Math.sin(-pointsDown[i].angle * Math.PI / 180) * pointsDown[i].dist;
+        pointsDown[i].x =
+          centerX +
+          radius *
+            Math.cos((-pointsDown[i].angle * Math.PI) / 180) *
+            pointsDown[i].dist;
+        pointsDown[i].y =
+          centerY +
+          radius *
+            Math.sin((-pointsDown[i].angle * Math.PI) / 180) *
+            pointsDown[i].dist;
       }
     }
 
@@ -162,29 +197,31 @@ const Visualizer = ({ forwardRef, close }) => {
     // return function cleanup () {
     //   // context.close();
     //   context.suspend();
+    //   // audio.source.
 
-    //   context.suspend().then(function() {
-    //     handlePlayerSuspend(true);
-    //   });
+    //   // context.suspend().then(function() {
+    //   //   handlePlayerSuspend(true);
+    //   // });
     // };
-
   }, []);
 
   return (
     <section className={styles.Visualizer}>
-      <svg 
-        className={styles.close} 
-        fill="none" 
-        stroke="currentColor" 
-        viewBox="0 0 24 24" 
+      <svg
+        className={styles.close}
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
-        height='1.5em'
-        onClick={close}>
-        <path 
-          strokeLinecap="round" 
-          strokeLinejoin="round" 
-          strokeWidth={2} 
-          d="M6 18L18 6M6 6l12 12" />
+        height="1.5em"
+        onClick={close}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M6 18L18 6M6 6l12 12"
+        />
       </svg>
       <canvas ref={canvasRef} />
     </section>
