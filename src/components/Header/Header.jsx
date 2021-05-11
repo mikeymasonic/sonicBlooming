31ccc883e82cdcac55c1f92600b56e0417ac07d6
Michael Romay <amerror@gmail.com>
2021-05-10 17:58:32 -0700

    trying this out

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -45,11 +45,12 @@ const Header = (props) => {
             stroke="rgba(40,4,78,1)"
             height="3em"
             viewBox="0 0 24 24" 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            strokeWidth={2} 
             xmlns="http://www.w3.org/2000/svg">
             <path 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-              strokeWidth={2} 
+              
               d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" >
             </path>
           </svg>
